be5b8a2fbe9b3fedb904602159d94fb63befc8cf
ParvathiNagaraj <103828406+ParvathiNagaraj@users.noreply.github.com>
2022-04-30 18:48:19 +0530

    Update Students.java

--- Students.java
+++ Students.java
@@ -7,32 +7,27 @@ public class Students {
 		// Object declaration for class
 		Students students = new Students();
 
-		int id = students.studentInfo(9000);
-		System.out.println("The id is " + id);
-
-		Object info = students.studentinfo(9000, "Parvathi");
-		System.out.println("The id and Name is " + info);
-
-		Object info1 = students.studentInfo("parudns3333@gmail.com", 992227778);
-		System.out.println("The emailid and PhoneNumber is " + info1);
+		students.studentInfo(9000);
+		students.studentinfo(9000, "Parvathi");
+		students.studentInfo("parudns3333@gmail.com", 992227778);
 
 	}
 
 	// creating method with name studentInfo
-	public int studentInfo(int id) {
+	public void studentInfo(int id) {
 
-		return id;
+		System.out.println("The id is " + id);
 	}
 
 	// overloading method with name studentInfo with diffent arguments
-	public Object studentinfo(int id, String name) {
+	public void studentinfo(int id, String name) {
 
-		return id + name;
+		System.out.println("The id and Name is " + id + " and " + name);
 
 	}
 
 	// overloading method with name studentInfo with diffent arguments
-	public Object studentInfo(String emailid, long phoneNumber) {
-		return emailid + phoneNumber;
+	public void studentInfo(String emailid, long phoneNumber) {
+		System.out.println("The emailid and PhoneNumber is " + emailid + " and " + phoneNumber);
 	}
 }
